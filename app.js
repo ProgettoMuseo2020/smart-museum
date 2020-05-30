89384f267b9eb68350fb299a74efc09ec41b95bf
ProgettoMuseo2020 <64857898+ProgettoMuseo2020@users.noreply.github.com>
2020-05-30 18:51:19 +0200

    Update app.js
    Controllo se la checkbox e uguale a "true" o "false". Se è uguale a "true" le opere diventano invisibili, invece se è uguale a "false" le opere sono visibili. Imposto anche un timer con un ritardo di 0.4 sec.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+function myFunction(){
+    var checkBox = document.getElementById("chk");
+    var opere = document.getElementById("opere");
 
+    if (checkBox.checked == true) {
+        opere.style.display = "none";
+
+    } else if (checkBox.checked == false){
+        setTimeout(function myFunction() {opere.style.display = "flex";}, 400); // Setto un timer
+    }
+}
